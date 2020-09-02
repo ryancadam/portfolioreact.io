dfceea031ae0cf6b5f1c30f704233e78aa0fd055
Ryan Adam <ryancadam88@gmail.com>
2020-09-02 17:02:45 -0500

    Updates

---
+++ precache-manifest.bbd5efab4aa6336c9bbad9db6dcecd8c.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "206b22bbee9df5371ffba5cd72149cab",
+    "url": "/portfolioreact.io/index.html"
+  },
+  {
+    "revision": "c59f4d92fd2104251200",
+    "url": "/portfolioreact.io/static/css/main.3f82fd26.chunk.css"
+  },
+  {
+    "revision": "f81191c062a96e9550d6",
+    "url": "/portfolioreact.io/static/js/2.438084b7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolioreact.io/static/js/2.438084b7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c59f4d92fd2104251200",
+    "url": "/portfolioreact.io/static/js/main.a102c62b.chunk.js"
+  },
+  {
+    "revision": "daa2566fc964ce77ead7",
+    "url": "/portfolioreact.io/static/js/runtime-main.d2b39f31.js"
+  },
+  {
+    "revision": "90efc6909f540ea7c211162ee6cfc16a",
+    "url": "/portfolioreact.io/static/media/blue.90efc690.svg"
+  },
+  {
+    "revision": "3dfc371d992bcd3e8b51d2ac2a220217",
+    "url": "/portfolioreact.io/static/media/me.3dfc371d.jpg"
+  },
+  {
+    "revision": "6b92cd50c0bc081c66691e87342b0cf5",
+    "url": "/portfolioreact.io/static/media/star.6b92cd50.svg"
+  }
+]);
